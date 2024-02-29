7226befb79d6d0c6e82522d2852eac9a04c7b267
kaltouma22 <kchbiki363@gmail.com>
2024-02-29 22:58:37 +0100

    Done

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,38 +1,57 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_right - Inserts a node as the right child of another node
- * @parent: Pointer to the node to insert the right child in
- * @value: Value to store in the new node
+ * binary_tree_node_2 - creates a binary tree node
+ * @parent: pointer to the parent node of the node to create
+ * @value: value to store in the new node
  *
- * Return: Pointer to the created node, or NULL on failure or if parent is NULL
- * If parent already has a right child, the new node takes its place,
- * and the old right child becomes the right child of the new node.
+ * Return: pointer to the new node
  */
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
-{
-	if (parent == NULL)
-	{
-		return (NULL);
-	}
 
+binary_tree_t *binary_tree_node_2(binary_tree_t *parent, int value)
+{
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node->n = value;
 	new_node->parent = parent;
+	new_node->n = value;
 	new_node->left = NULL;
-	new_node->right = parent->right;
+	new_node->right = NULL;
 
-	if (parent->right != NULL)
-	{
-		parent->right->parent = new_node;
-	}
-
-	parent->right = new_node;
 	return (new_node);
+}
+
+/**
+ * binary_tree_insert_right - inserts a node as the right-child of another node
+ * @parent: pointer to the node to insert the right-child in
+ * @value: value to store in the new node
+ *
+ * Description: If parent already has a right-child, the new node must take its
+ * place and the old right-child must be set as the right-child of the new node
+ *
+ * Return: pointer to the created node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *right_child;
+
+	if (parent == NULL)
+		return (NULL);
+
+	right_child = binary_tree_node_2(parent, value);
+
+	if (right_child == NULL)
+		return (NULL);
+
+	right_child->right = parent->right;
+
+	if (right_child->right != NULL)
+		right_child->right->parent = right_child;
+
+	parent->right = right_child;
+
+	return (right_child);
 }
